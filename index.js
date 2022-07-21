5840a66358f96462a82d5d2608162e65534c5741
TARANSH CHELLANI <99187588+Taransh121@users.noreply.github.com>
2022-07-21 09:48:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let screen=document.getElementById("screen")
+let buttons=document.querySelectorAll("button")
+screenValue="";
+
+for(item of buttons){
+    item.addEventListener("click",(e)=>{
+        buttonText=e.target.innerText;
+        // console.log(buttonText);
+        if(buttonText=="CE" || buttonText=="ON"){
+            screenValue="";
+            screen.value=screenValue;
+        }
+        else if(buttonText=="="){
+            screen.value=eval(screenValue)
+        }
+        else{
+            screenValue+=buttonText;
+            screen.value=screenValue
+        }
+    })
+
+
+}
